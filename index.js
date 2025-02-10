6e57ba0994e057c76ecd60189a71d1e84a957b9b
lucasbasombrio <basombrio.lucas5@gmail.com>
2025-02-10 15:53:21 -0300

    hice un final modelo para tenerlo de base

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from "express";
+import routes from "./routes/routes.js";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(routes);
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-
-
-app.listen(8080, ()=>{
-    console.log("🚀 ~ app.listen ~ 8080")
-    
-})
+app.listen(8080, () => {
+  console.log("🚀 ~ app.listen ~ 8080");
+});
